6ea38b745c09fe7ff4fd085d64af15db89061f78
siswabsen-ops <siswabsen@gmail.com>
2026-08-18 06:16:07 +0700

    Add files via upload

---
+++ ai_studio_code (1).sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Sinkronisasi versi terbaru DIGIWANGI 3"
+git branch -M main
+git remote add origin https://github.com/USERNAME/REPO_NAME.git
+git push -u origin main
